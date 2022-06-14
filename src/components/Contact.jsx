adc0e991d3a6c4ab6d0ac2d53246a8cf610fefe5
Daniel Simonelli <63931248+dsimonel@users.noreply.github.com>
2022-06-13 22:30:44 -0300

    New commit

---
+++ src/components/Contact.jsx
@@ -0,0 +1,141 @@
+import { useEffect, useState } from "react";
+// import Loader from "react-loaders";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import AnimatedLetters from "./AnimatedLetters";
+import { useTranslation } from "react-i18next";
+import "./Contact.scss";
+
+const Contact = () => {
+  const { t } = useTranslation();
+  const [letterClass, setLetterClass] = useState("text-animate");
+  const formRef = useRef();
+
+  const contacto = t("contact");
+  let contStr = [];
+  for (let i = 0; i < contacto.length; i++) {
+    contStr.push(contacto.charAt(i));
+  }
+
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass("text-animate-hover");
+    }, 3000);
+  }, []);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_ruoppbo",
+        "template_jp7eobg",
+        formRef.current,
+        "AhFuuH51hDZWlaVB9"
+      )
+      .then(
+        () => {
+          alert(t("msg_ok"));
+          window.location.reload(false);
+        },
+        () => {
+          alert(t("msg_fail"));
+        }
+      );
+  };
+
+  return (
+    <div name="contact" className="w-full h-screen bg-[#0a192f] text-gray-300">
+      {/* <div className="flex flex-col justify-center items-center w-full h-screen"> */}
+      <div className="max-w-[1500px] mx-auto p-4 flex flex-col justify-center w-full h-screen">
+        <div className="grid sm:grid-cols-2 md:grid-cols-2 gap-1">
+          <div className="containerqqq contact-page">
+            <div className="text-zone sm:w-full md:w-10/12 md:left-20">
+              <h1>
+                {" "}
+                &nbsp;
+                <AnimatedLetters
+                  letterClass={letterClass}
+                  strArray={contStr}
+                  idx={15}
+                />
+              </h1>
+              <p>{t("interest")}</p>
+              <br></br>
+              <div className="contact-form">
+                <form ref={formRef} onSubmit={sendEmail}>
+                  <ul>
+                    <li className="half">
+                      <input
+                        placeholder={t("name")}
+                        type="text"
+                        name="from_name"
+                        required
+                      />
+                    </li>
+                    <li className="half">
+                      <input
+                        placeholder="Email"
+                        type="email"
+                        name="from_email"
+                        required
+                      />
+                      <input
+                        hidden
+                        type="email"
+                        name="to_email"
+                        defaultValue="dsimonel@gmail.com"
+                      />
+                    </li>
+                    <li>
+                      <input
+                        placeholder={t("subject")}
+                        type="text"
+                        name="user_subject"
+                        required
+                      />
+                    </li>
+                    <li>
+                      <textarea
+                        placeholder={t("message")}
+                        name="message"
+                        required
+                      ></textarea>
+                    </li>
+                    <li>
+                      <input
+                        type="submit"
+                        className="flat-button"
+                        value={t("send")}
+                      />
+                    </li>
+                  </ul>
+                </form>
+              </div>
+            </div>
+            {/* <div className="info-map">
+            Daniel Simonelli
+            <br />
+            Calle 497 n° 754, B7608FUR <br />
+            Mar del Plata / Argentina <br />
+            <br />
+            <span>dsimonel@gmail.com</span>
+          </div> */}
+          </div>
+          <div className="map-wrap">
+            <MapContainer center={[-38.116362, -57.602241]} zoom={13}>
+              <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+              <Marker position={[-38.116362, -57.602241]}>
+                <Popup>{t("headquarters")}</Popup>
+              </Marker>
+            </MapContainer>
+          </div>
+        </div>
+        {/* <Loader type="pacman" /> */}
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
---
+++ src/components/Work.jsx
@@ -0,0 +1,88 @@
+import React from "react";
+import WorkItem from "./WorkItem";
+import fpmt from "../assets/fpmt.jpg";
+import Booking from "../assets/Booking.jpg";
+// import MLR from "../assets/MLR.jpg";
+import mymusic from "../assets/mymusic-4.jpg";
+import TP from "../assets/TP.jpg";
+import myadmin from "../assets/myadmin.jpg";
+import CoinMarketCap from "../assets/CMC-2.jpg";
+import { useTranslation } from "react-i18next";
+
+const Work = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+      <div className="max-w-[1300px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            {t("work")}
+          </p>
+          <p className="py-6 text-2xl">{t("checkout")}</p>
+        </div>
+
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid Item */}
+          <WorkItem
+            imgLink={fpmt}
+            workType={t("phpapp")}
+            workDesc={t("wd1")}
+            firstButton={"Demo"}
+            secondButton={"Website"}
+            href1={"https://youtu.be/7oPGP7NI-1o"}
+            href2={"https://tibetan.ueuo.com/fpmt/guest.php"}
+          />
+          <WorkItem
+            imgLink={CoinMarketCap}
+            workType={"Solidity/React/Tailwind"}
+            workDesc={t("wd2")}
+            firstButton={t("code")}
+            secondButton={"Website"}
+            href1={"https://github.com/dsimonel/web3_coinmarketcap"}
+            href2={"http://danielsimonelli.orgfree.com/cmc/index.html"}
+          />
+          <WorkItem
+            imgLink={TP}
+            workType={t("phpapp")}
+            workDesc={t("wd3")}
+            firstButton={"Code"}
+            secondButton={"Website"}
+            href1={"#"}
+            href2={"http://tashipedeb.ueuo.com/index.php"}
+          />
+          <WorkItem
+            imgLink={myadmin}
+            workType={"React JS/Tailwind"}
+            workDesc={t("wd4")}
+            firstButton={t("code")}
+            secondButton={"Website"}
+            href1={"https://github.com/dsimonel/admin-react"}
+            href2={"https://dsimonel.github.io/admin-react"}
+          />
+          <WorkItem
+            imgLink={mymusic}
+            workType={"React/Node/Mongo DB"}
+            workDesc={t("wd5")}
+            firstButton={"Demo"}
+            secondButton={"Website"}
+            href1={"https://www.youtube.com/watch?v=qN_JH_10i6k&t=58s"}
+            href2={"http://danielsimonelli.orgfree.com/mymusic/index.html"}
+          />
+          <WorkItem
+            imgLink={Booking}
+            workType={"React/MongoDB"}
+            workDesc={t("wd6")}
+            firstButton={t("code")}
+            secondButton={"Website"}
+            href1={"https://github.com/dsimonel/mybooking-ui-react"}
+            href2={"https://dsimonel.github.io/mybooking-react/"}
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Work;
